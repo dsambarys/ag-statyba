a0ce5a3600ecac80c71a3dd5f4579dca83561255
dxszamb <dsambarys@gmail.com>
2025-05-31 16:33:44 +0300

    fitn0erb9u

--- src/lib/components/PostListing.svelte
+++ src/lib/components/PostListing.svelte
@@ -8,10 +8,8 @@
 </script>
 
 <article class="p-4 sm:flex sm:space-x-4">
-  <a {href} class="block sm:w-80">
-    <div
-      class="aspect-w-16 aspect-h-9 rounded-lg overflow-hidden"
-    >
+  <a href={href} class="block sm:w-80">
+    <div class="aspect-w-16 aspect-h-9 rounded-lg overflow-hidden">
       <img
         src={post.metadata.imgUrl}
         alt={post.metadata.title}
@@ -21,27 +19,23 @@
   </a>
 
   <div class="flex-1 py-2 sm:py-0">
-    <a {href}>
+    <a href={href}>
       <h3 class="text-xl font-medium mb-1">
         {post.metadata.title}
       </h3>
-      <p class="font-light text-gray-60 dark:text-gray-300">
+      <p class="font-light text-gray-600 dark:text-gray-300">
         <span>Published: </span>
         <time datetime={post.metadata.publishedAt}>
           {formatPublishedAt(post.metadata.publishedAt)}
         </time>
       </p>
-
       <p class="py-2 font-light">
         {post.metadata.summary}
       </p>
     </a>
 
-    <div
-      class="flex space-x-4 text-gray-600 dark:text-gray-300 font-light
-          underline"
-    >
-      <a {href}>Read More</a>
+    <div class="flex space-x-4 text-gray-600 dark:text-gray-300 font-light underline">
+      <a href={href}>Sužinokite daugiau</a>
     </div>
   </div>
 </article>
